6dd9c9806fd49dda1f7ef5108988929f51db58e1
Sarunas <sverunas@gmail.com>
2023-06-19 15:53:44 +0300

    backas

---
+++ back-end/mishaPay/src/main/java/com/project/mishaPay/category/Category.java
@@ -0,0 +1,33 @@
+package com.project.mishaPay.category;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+
+public class Category {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "category")
+	private String name;
+
+//	@JsonIgnore
+//	@OneToMany(mappedBy = "category")
+//	@Getter
+//	private List<Expense> expense;
+
+}
---
+++ back-end/mishaPay/src/main/java/com/project/mishaPay/dto/IncomeDTO.java
@@ -0,0 +1,15 @@
+package com.project.mishaPay.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import lombok.Data;
+
+@Data
+public class IncomeDTO {
+
+	private Long id;
+	private BigDecimal amount;
+	private String name;
+	private LocalDate date;
+}
